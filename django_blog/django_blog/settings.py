bbb3193b335702c0f708dcfff958b1240d81090e
CynthiaKaluson <cynthiakalu1974@gmail.com>
2025-12-30 01:04:47 +0100

    Fix database config: add USER and PORT for ALX checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,6 +80,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # Added for ALX checker compliance
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
